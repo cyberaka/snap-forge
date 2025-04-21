b58346b7a9187660389e30527053362832ef7f8d
Abhinav Anand <cyberaka@gmail.com>
2025-04-21 17:06:32 +0530

    A working chatgpt integration service added.

--- snap-forge-service/src/main/java/com/cyberaka/snapforge/SnapForgeApplication.java
+++ snap-forge-service/src/main/java/com/cyberaka/snapforge/SnapForgeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiChatApplication {
+public class SnapForgeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringAiChatApplication.class, args);
+        SpringApplication.run(SnapForgeApplication.class, args);
     }
 }
